c78c2ce05bf49ca6b1612d0dc5eca9ab724d8f5e
mahmoudaljali00 <mahmoudaljali00@gmail.com>
2025-06-07 22:44:34 +0300

    vv

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: "/admin/",
-        destination: "https://seaside-backend.vercel.app/",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
