65e593ceb38a1cc0ec73ea328b0d6b4b7ead6dd7
mahmoudaljali00 <mahmoudaljali00@gmail.com>
2025-06-07 21:18:09 +0300

    nv

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
-import nextConfig from './next.config.js'; // Instead of require()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
-
-module.exports = {
   async rewrites() {
     return [
       {
